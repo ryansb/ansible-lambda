e12cff4aa9c8927adf22152a3644c7f3520e4cac
Pierre Jodouin <phjodouin@gmail.com>
2015-12-20 16:43:04 -0500

    standardised error messages

--- lambda_facts.py
+++ lambda_facts.py
@@ -6,52 +6,43 @@ AWS Lambda WIP
 
 try:
     import boto3
+    from boto.exception import *
+    from botocore.exceptions import *
     HAS_BOTO3 = True
 except ImportError:
     HAS_BOTO3 = False
 
 
 def all_details(client, module):
+
+    if module.params.get('max_items') or module.params.get('next_marker'):
+        module.fail_json(msg='Cannot specify max_items nor next_marker for query=all.')
+
     lambda_facts = dict()
 
-    function_name = module.params['function_name']
-
+    function_name = module.params.get('function_name')
     if function_name:
-        try:
-            lambda_facts.update(client.get_function_configuration(FunctionName=function_name))
-            lambda_facts.update(client.list_aliases(FunctionName=function_name))
-            # get_policy returns a JSON string so must convert to dict before reassigning to its key
-            lambda_facts.update(Policy=json.loads(client.get_policy(FunctionName=function_name)['Policy']))
-
-            lambda_facts.update(client.list_versions_by_function(FunctionName=function_name))
-        except Exception as e:
-            module.fail_json(msg=str(e))
+        lambda_facts.update(config_details(client, module))
+        lambda_facts.update(alias_details(client, module))
+        lambda_facts.update(policy_details(client, module))
+        lambda_facts.update(version_details(client, module))
     else:
-        params = dict()
-        if module.params.get('max_items'):
-            params['MaxItems'] = module.params.get('max_items')
-
-        if module.params.get('next_marker'):
-            params['Marker'] = module.params.get('next_marker')
-
-        try:
-            lambda_facts.update(client.list_functions(**params))
-            lambda_facts.update(client.list_event_source_mappings(**params))
-        except Exception as e:
-            module.fail_json(msg=str(e))
+        lambda_facts.update(config_details(client, module))
+        lambda_facts.update(mapping_details(client, module))
 
     return lambda_facts
 
 
 def config_details(client, module):
+
     lambda_facts = dict()
 
-    function_name = module.params['function_name']
+    function_name = module.params.get('function_name')
     if function_name:
         try:
             lambda_facts.update(client.get_function_configuration(FunctionName=function_name))
-        except Exception as e:
-            module.fail_json(msg=str(e))
+        except (BotoServerError, ClientError), e:
+            module.fail_json(msg='Unable to get {0} configuration, error: {1}'.format(function_name, e))
     else:
         params = dict()
         if module.params.get('max_items'):
@@ -62,41 +53,56 @@ def config_details(client, module):
 
         try:
             lambda_facts.update(client.list_functions(**params))
-        except Exception as e:
-            module.fail_json(msg=str(e))
-
+        except (BotoServerError, ClientError), e:
+            module.fail_json(msg='Unable to get function list, error: {0}'.format(e))
 
     return lambda_facts
 
 
 def alias_details(client, module):
+
     lambda_facts = dict()
 
-    function_name = module.params['function_name']
+    function_name = module.params.get('function_name')
     if function_name:
+        params = dict()
+        if module.params.get('max_items'):
+            params['MaxItems'] = module.params.get('max_items')
+
+        if module.params.get('next_marker'):
+            params['Marker'] = module.params.get('next_marker')
         try:
-            lambda_facts.update(client.list_aliases(FunctionName=function_name))
-        except Exception as e:
-            module.fail_json(msg=str(e))
+            lambda_facts.update(client.list_aliases(FunctionName=function_name, **params))
+        except (BotoServerError, ClientError), e:
+            module.fail_json(msg='Unable to get {0} aliases, error: {1}'.format(function_name, e))
+    else:
+        module.fail_json(msg='Parameter function_name required for query=aliases.')
 
     return lambda_facts
 
 
 def policy_details(client, module):
+
+    if module.params.get('max_items') or module.params.get('next_marker'):
+        module.fail_json(msg='Cannot specify max_items nor next_marker for query=policy.')
+
     lambda_facts = dict()
 
-    function_name = module.params['function_name']
+    function_name = module.params.get('function_name')
     if function_name:
         try:
             # get_policy returns a JSON string so must convert to dict before reassigning to its key
             lambda_facts.update(Policy=json.loads(client.get_policy(FunctionName=function_name)['Policy']))
-        except Exception as e:
-            module.fail_json(msg=str(e))
+        except (BotoServerError, ClientError), e:
+            module.fail_json(msg='Unable to get {0} policy, error: {1}'.format(function_name, e))
+    else:
+        module.fail_json(msg='Parameter function_name required for query=policy.')
 
     return lambda_facts
 
 
 def mapping_details(client, module):
+
     lambda_facts = dict()
 
     if module.params.get('function_name'):
@@ -111,7 +117,7 @@ def mapping_details(client, module):
 
     try:
         lambda_facts.update(client.list_event_source_mappings(**params))
-    except Exception as e:
+    except (BotoServerError, ClientError), e:
         module.fail_json(msg=str(e))
 
 
@@ -119,13 +125,21 @@ def mapping_details(client, module):
 
 
 def version_details(client, module):
+
     lambda_facts = dict()
 
-    function_name = module.params['function_name']
+    function_name = module.params.get('function_name')
     if function_name:
+        params = dict()
+        if module.params.get('max_items'):
+            params['MaxItems'] = module.params.get('max_items')
+
+        if module.params.get('next_marker'):
+            params['Marker'] = module.params.get('next_marker')
+
         try:
-            lambda_facts.update(client.list_versions_by_function(FunctionName=function_name))
-        except Exception as e:
+            lambda_facts.update(client.list_versions_by_function(FunctionName=function_name, **params))
+        except (BotoServerError, ClientError), e:
             module.fail_json(msg=str(e))
 
     return lambda_facts
@@ -146,29 +160,23 @@ def main():
     module = AnsibleModule(
         argument_spec=argument_spec,
         supports_check_mode=True,
-        mutually_exclusive=[
-            ['function_name', 'max_items'],
-            ['show_versions', 'max_items'],
-            ['mappings', 'function_name']
-        ],
-        required_together=[
-
-        ]
+        mutually_exclusive=[],
+        required_together=[]
     )
 
     # validate dependencies
     if not HAS_BOTO3:
-        module.fail_json(msg='boto3 required for this module and can coexist with boto.')
+        module.fail_json(msg='Both boto and boto3 are required for this module.')
 
     # validate function_name if present
     function_name = module.params['function_name']
     if function_name:
-        if not re.search('^[a-zA-z][a-zA-Z0-9\-_]+$', function_name):
-            module.fail_json(msg='Function name %s is invalid. Names must contain only alphanumeric characters and hyphens.' % function_name)
-
+        if not re.search('^[\w\-]+$', function_name):
+            module.fail_json(
+                    msg='Function name {0} is invalid. Names must contain only alphanumeric characters and hyphens.'.format(function_name)
+            )
         if len(function_name) > 64:
-            module.fail_json(msg='Function name "%s" exceeds 64 character limit' % function_name)
-
+            module.fail_json(msg='Function name "{0}" exceeds 64 character limit'.format(function_name))
 
     client = boto3.client('lambda')
 
